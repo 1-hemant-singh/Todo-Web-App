f24a5465f19f26b2753c540c2323296628ba2b3d
1-hemant-singh <123456hemantsingh@gmail.com>
2025-06-07 16:59:31 +0530

    first upload

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { use, useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { TodoProvider } from './contexts';
+import TodoItem from './components/TodoItem';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => (todo.id != id)))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? { ...prevTodo, completed: !prevTodo.completed } : prevTodo))
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+  return (
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
+      <div className="bg-[#172842] min-h-screen py-8 px-4">
+        <div className="w-full mx-auto shadow-md rounded-lg px-6 py-4 text-white bg-[#1e293b]">
+          <h1 className="text-3xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-6">
+            <TodoForm />
+          </div>
+          <div className="flex flex-col gap-y-4">
+            {todos.map((todo) => (
+              <div className="w-full" key={todo.id}>
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  )
+}
+
+export default App
